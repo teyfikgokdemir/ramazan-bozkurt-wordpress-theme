cebd85c80925949fff42438b0e47b9a377320ded
teyfikgokdemir <teyfikgokdemir@outlook.com>
2026-09-10 22:51:11 +0300

    Improve media lookup and enqueue theme interactions

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@ function rb_theme_setup() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('custom-logo', [
-        'height' => 120,
-        'width' => 320,
+        'height' => 160,
+        'width' => 420,
         'flex-height' => true,
         'flex-width' => true,
     ]);
@@ -21,7 +21,9 @@ function rb_theme_setup() {
 add_action('after_setup_theme', 'rb_theme_setup');
 
 function rb_enqueue_assets() {
-    wp_enqueue_style('rb-style', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('rb-style', get_stylesheet_uri(), [], $version);
+    wp_enqueue_script('rb-site', get_template_directory_uri() . '/assets/js/site.js', [], $version, true);
 }
 add_action('wp_enqueue_scripts', 'rb_enqueue_assets');
 
@@ -30,5 +32,23 @@ function rb_get_media_url_by_slug($slug) {
     if ($attachment) {
         return wp_get_attachment_image_url($attachment->ID, 'full');
     }
+
+    $query = new WP_Query([
+        'post_type'      => 'attachment',
+        'post_status'    => 'inherit',
+        'posts_per_page' => 1,
+        'name'           => sanitize_title($slug),
+        'fields'         => 'ids',
+    ]);
+
+    if (!empty($query->posts[0])) {
+        return wp_get_attachment_image_url((int) $query->posts[0], 'full');
+    }
+
     return '';
+}
+
+function rb_media($slug, $fallback = '') {
+    $url = rb_get_media_url_by_slug($slug);
+    return $url ?: $fallback;
 }
