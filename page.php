e2fb72bbaa47fbb36cb5fed3eb511982c59fb6cc
teyfikgokdemir <teyfikgokdemir@outlook.com>
2026-09-10 23:35:22 +0300

    Improve flexible premium page template

--- page.php
+++ page.php
@@ -4,11 +4,14 @@
   <div class="rb-container">
     <div class="rb-eyebrow">Ramazan Bozkurt Et Ürünleri</div>
     <h1><?php the_title(); ?></h1>
+    <?php if (has_excerpt()) : ?><p><?php echo esc_html(get_the_excerpt()); ?></p><?php endif; ?>
   </div>
 </section>
 <section class="rb-page-copy">
   <div class="rb-container">
-    <?php the_content(); ?>
+    <article <?php post_class('rb-entry'); ?>>
+      <?php the_content(); ?>
+    </article>
   </div>
 </section>
 <?php endwhile; ?>
